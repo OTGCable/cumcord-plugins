e20f9fdc46ff70ee969cdfd3787c2e49728c01bd
Phorcys <57866459+phorcys420@users.noreply.github.com>
2021-11-07 11:42:38 +0100

    Make code not shit
    [+] Make code not shit
    [+] ELRICK

--- TrueTrueIAgreeWithYourStatement/truetrue.js
+++ TrueTrueIAgreeWithYourStatement/truetrue.js
@@ -2,50 +2,43 @@ import { findByProps, findAll } from "@cumcord/modules/webpack"
 const { getChannelId } = findByProps("getChannelId", "getVoiceChannelId");
 const messageQueue = findAll(arg => arg.enqueue)[0]
 
-const keyHandler = async (event) => {
-    if (event.code == "Shift") + (event.code == "5") {
-        event.preventDefault();
-        messageQueue.enqueue(
-            {
-                "type": 0,
-                "message": {
-                    channelId: getChannelId(),
-                    content: "https://tenor.com/view/funny-gif-23169978"
-                }
-            },
-            r => {
-                return;
+function enqueueMessage(text) {
+    return messageQueue.enqueue(
+        {
+            "type": 0,
+            "message": {
+                channelId: getChannelId(),
+                content: text
             }
-        )
-    }
+        },
+        r => {
+            return;
+        }
+    )
 }
 
-const keyHandler = async (event) => {
-    if (event.code == "Shift") + (event.code == "6") {
+// Ideally we would want to be able to remap the keys but i'm just here to make this functional and not shit
+function keyHandler(event) {
+    if(event.altKey == true && event.keyCode == 70) { // ALT + F - Dog
         event.preventDefault();
-        messageQueue.enqueue(
-            {
-                "type": 0,
-                "message": {
-                    channelId: getChannelId(),
-                    content: "https://i.imgur.com/kJonTcq.png"
-                }
-            },
-            r => {
-                return;
-            }
-        )
+        return enqueueMessage("https://tenor.com/view/funny-gif-23169978");
+    } else if(event.altKey == true && event.keyCode == 72) { // ALT + H - Beef
+        event.preventDefault();
+        return enqueueMessage("https://i.imgur.com/kJonTcq.png");
+    } else if(event.altKey == true && event.keyCode == 69) { // ALT + E - el rick
+        event.preventDefault();
+        return enqueueMessage(`https://${(Math.random() + 1).toString(36).substring(2)}.elrick.fans`);
     }
+
+    return event;
 }
 
 export default (data) => {
     return {
         onLoad() {
-
             document.addEventListener("keydown", keyHandler)
         },
         onUnload() {
-            
             document.removeEventListener("keydown", keyHandler)
         }
     }
